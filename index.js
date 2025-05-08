c60a0876287f8df5b5c55d642a6ca8cf106720a7
abhiroy00 <abhi7464099@gmail.com>
2025-05-08 14:30:11 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,106 @@
+// num1=10
+// console.log(typeof(num1));
+// ===================================
+
+// name1="abhi"
+// console.log(typeof(name1));
+// =====================================>
+
+// ans=true
+// console.log(typeof(ans));
+
+// =======================================>
+
+    // 1 arthmatic oprator
+
+    // +
+    // -
+    // *
+    // /
+    // %
+
+// console.log(20+20);
+// console.log(40-20);
+// console.log(5*5);
+// console.log(21%2);
+// console.log(21/2);
+// console.log(Math.floor(21/2));
+// console.log(Math.ceil(21/2));
+// ========================================
+
+// camparision
+
+// >
+// <
+// >=
+// <=
+// == 
+// !=
+
+// console.log(9>2);
+// console.log(8<2);
+// console.log(9>=9);
+// console.log(9<=9);
+// console.log(9==9);
+// console.log(9!=9);
+// =======================================>
+
+// logical oprator
+// AND 
+// OR 
+// NOT 
+
+// AND
+
+// A             B      A*B=C 
+// T             F        F 
+// F             T        F 
+// F             F        F 
+// T             T        T    
+// console.log(8>2 && 8>19);
+
+// OR
+
+// A              B       A+B=C 
+// T             F         T  
+// F             T         T 
+// T             T         T 
+// F             F         F 
+
+// console.log(9>2 || 7>19);
+
+// NOT 
+
+// A           B
+// T           F 
+// F           T
+
+// ans=true
+// console.log(!ans);
+
+
+// ans=(7<2) ?console.log("hii"):console.log("hello");
+
+
+
+
+
+
+
+
+
+
+
+
+
+// =========================================>
+
+
+
+
+
+
+
+
+
+
